4ede0dc6acc9be0434c9025669c371cf390e0347
Justin Daniel <justin.daniel@swri.org>
2020-09-23 08:54:17 -0500

    fixed headers to build in melodic

--- apriltag_ros/src/common_functions.cpp
+++ apriltag_ros/src/common_functions.cpp
@@ -32,15 +32,15 @@
 #include "apriltag_ros/common_functions.h"
 #include "image_geometry/pinhole_camera_model.h"
 
-#include "common/homography.h"
-#include "tagStandard52h13.h"
-#include "tagStandard41h12.h"
-#include "tag36h11.h"
-#include "tag25h9.h"
-#include "tag16h5.h"
-#include "tagCustom48h12.h"
-#include "tagCircle21h7.h"
-#include "tagCircle49h12.h"
+#include <apriltag/common/homography.h>
+#include <apriltag/tagStandard52h13.h>
+#include <apriltag/tagStandard41h12.h>
+#include <apriltag/tag36h11.h>
+#include <apriltag/tag25h9.h>
+#include <apriltag/tag16h5.h>
+#include <apriltag/tagCustom48h12.h>
+#include <apriltag/tagCircle21h7.h>
+#include <apriltag/tagCircle49h12.h>
 
 namespace apriltag_ros
 {
